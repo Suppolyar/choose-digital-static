c9e0dfd4c1f75d7761f60efd208d1f475108abe0
suppolyar <vkhardy@inbox.ru>
2021-07-21 17:02:51 +0300

    17:02 Desktop v.

---
+++ src/slick/config.rb
@@ -0,0 +1,17 @@
+css_dir = "."
+sass_dir = "."
+images_dir = "."
+fonts_dir = "fonts"
+relative_assets = true
+
+output_style = :compact
+line_comments = false
+
+preferred_syntax = :scsselative
+    max-width: 1500px
+    margin: 0 auto
+
+@import 'blocks/_header'
+@import 'blocks/_main'
+@import 'blocks/_slider'
+@import 'blocks/_footer'
