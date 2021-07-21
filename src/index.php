81b267207c287400ae37905f88fac625d91c4a39
suppolyar <vkhardy@inbox.ru>
2021-07-21 19:45:33 +0300

    v1.1

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/slick.css"/>
     <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?=time()?>">
     <link rel="stylesheet" href="css/fonts.css">
     
 </head>
@@ -161,7 +161,7 @@
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="slick/slick.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js?v=<?=time()?>"></script>
 
 </body>
 </html>
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+$('.multiple-items').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
